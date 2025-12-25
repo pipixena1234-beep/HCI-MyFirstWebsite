b98b43e5b30a51d1821adfca9c74452c7b8e3a40
pipixena1234-beep <pipixena1234@gmail.com>
2025-12-25 16:47:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,7 +185,7 @@ if uploaded_file:
                     }
                     term_folder = drive_service.files().create(
                     body=folder_metadata,
-                    fields='id'
+                    fields='id',
                     supportsAllDrives=True
                     ).execute()
                     term_folder_id = term_folder['id']
