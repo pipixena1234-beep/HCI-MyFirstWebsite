2e6cc9639fc14ac9b3f38ef0478eed74a5c8744b
pipixena1234-beep <pipixena1234@gmail.com>
2025-12-26 20:58:33 +0800

    Update automated_upload.py

--- automated_upload.py
+++ automated_upload.py
@@ -7,6 +7,7 @@ from io import BytesIO
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
+import io  # <--- MISSING IMPORT FIXED
 
 # --- Helper: Flatten Logic (Same as your Streamlit version) ---
 def extract_and_flatten(df_raw):
